b8497caf2fb35ad1f410efad12c84949468a6594
hugolpz <hugo.lpz@gmail.com>
2021-04-04 22:22:24 +0200

    first egg

---
+++ wiki-editmany.js
@@ -0,0 +1,26 @@
+
+// commons-login.js : I just loging into the target wiki.
+const Wikiapi= require('wikiapi');
+const logins = require('./logins.js');
+
+// Login credentials from .login*.js
+var USER = logins.commons.user,
+	PASS = logins.commons.pass,
+	API  = logins.commons.api;
+
+(async () => {
+    const targetWiki = new Wikiapi;
+    await targetWiki.login(USER, PASS, API);
+    console.log(`Username ${USER} is connected !`);
+
+    /*** CORE ACTION(S) HERE : REPLACE ME ! ********** */
+    /**/   var listPages = [ 'Sandbox_1', 'Sandbox_2', 'Sandbox_3'];
+    /**/   // Add template {stub}, replace-remove vandalism if any, add category.
+    /**/   await wiki.for_each_page(
+    /**/       listPages, 
+    /**/       page_data => { return `{{stub}}\n`+page_data.wikitext.replace(/^/g,'Thanos says: ')+`\n[[Category:Bot test: edit]]`; }, // new content
+    /**/           {bot: 1, nocreate: 0, minor: 1, summary: 'Bot test: edit'}  // edit options
+    /**/   );
+    /*** END ***************************************** */
+
+})();
