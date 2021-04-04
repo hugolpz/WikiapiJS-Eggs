bd366d4eb8dccabad7e09a154a0fa40bafae3dc9
hugolpz <hugo.lpz@gmail.com>
2021-04-04 23:46:23 +0200

    create wiki-read-many.js

---
+++ wiki-read-many.js
@@ -0,0 +1,32 @@
+// PURPOSE: Script reads targets, print wikicode content into text files.
+// Run: $node wiki-upload-many.js
+const Wikiapi= require('wikiapi');
+const logins = require('./logins.js');
+const fs = require('fs');
+
+// Login credentials from .login*.js
+var USER = logins.commons.user,
+	PASS = logins.commons.pass,
+	API  = logins.commons.api;
+
+(async () => {
+    const targetWiki = new Wikiapi;
+    await targetWiki.login(USER, PASS, API);
+    console.log(`Username ${USER} is connected !`);
+
+/* *************************************************************** */
+/* CORE ACTION(S) HERE : HACK ME ! ******************************* */
+    var listPages = [ 'Unicorn', 'Azure Dragon', 'Leviathan'];
+    // Add template {stub}, replace-remove vandalism if any, add category.
+    await wiki.for_each_page(
+        listPages, 
+        d => {            
+            console.log(d.wikitext);
+            fs.writeFileSync(d.title+'.txt', d.wikitext, { encoding:'utf8'});
+            return Wikiapi.skip_edit;
+        } // skip edit step
+   );
+/* END CORE ****************************************************** */
+/* *************************************************************** */
+
+})();
