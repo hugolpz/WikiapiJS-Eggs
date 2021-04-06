2a07bd9eb1e37bd93d5bd2a3be86473dd6745da8
hugolpz <hugo.lpz@gmail.com>
2021-04-06 09:21:31 +0200

    minor adjustment

--- data/letters.js
+++ data/letters.js
@@ -1,8 +1,8 @@
 module.exports = [
-    { letter: 'a', filename: 'a-red.png', author: 'Beyoncey' },
-    { letter: 'b', filename: 'b-red.png', author: 'Beyoncey' },
-    { letter: 'c', filename: 'c-red.png', author: 'Shakiraa' },
-    { letter: 'd', filename: 'd-red.png', author: 'Beyoncey' },
-    { letter: 'e', filename: 'e-red.png', author: 'Queen Elisabeth II' },
-    { letter: 'f', filename: 'f-red.png', author: 'Beyoncey' },
+    { letter: 'a', filename: 'a-letter-test.svg', author: 'Beyoncey' },
+    { letter: 'b', filename: 'b-letter-test.svg', author: 'Beyoncey' },
+    { letter: 'c', filename: 'c-letter-test.svg', author: 'Shakiroar' },
+    { letter: 'd', filename: 'd-letter-test.svg', author: 'Beyoncey' },
+    { letter: 'e', filename: 'e-letter-test.svg', author: 'Queen Elisabeth II' },
+    { letter: 'f', filename: 'f-letter-test.svg', author: 'Shakiroar' },
 ];
